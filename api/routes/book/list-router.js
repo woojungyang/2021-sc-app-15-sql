94e56f65e9155a0a0fa6ae6c820ed8cced1780f2
woojungyang <blossom1113@nate.com>
2021-10-19 00:55:16 +0900

    patch view

--- api/routes/book/list-router.js
+++ api/routes/book/list-router.js
@@ -12,7 +12,8 @@ router.get(['/', '/:page'],isApiUser, async (req, res, next) => {
 	try {
 		const { count: totalRecord } = await findBookCount()
 		const page = Number(req.params.page || 1)
-		const pager = createPager(page, totalRecord, 5, 3)
+		const { listCnt,pagerCnt } = req.query;
+		const pager = createPager(page, totalRecord, listCnt, pagerCnt)
 		const { books } = await findBooks(pager.startIdx.toString(), pager.listCnt.toString())
 		books.forEach(v => {
 			v.createdAt = moment(v.createdAt).format('YYYY-MM-DD')
--- back/public/js/book/view.js
+++ back/public/js/book/view.js
@@ -1,11 +1,14 @@
-document.querySelector('#btDelete').addEventListener('click', onDelete)
-function onDelete(e) {
-	if(confirm(this.dataset['msg'])) {
-		document.deleteForm.submit();
+if(document.querySelector('#btDelete')){
+	document.querySelector('#btDelete').addEventListener('click', onDelete)
+	function onDelete(e) {
+		if(confirm(this.dataset['msg'])) {
+			document.deleteForm.submit();
+		}
 	}
 }
-
-document.querySelector('#btUpdate').addEventListener('click', onUpdate)
-function onUpdate(e) {
-	location.href = '/'+this.dataset['lang']+'/book/form/' + this.dataset['idx'];
+if(document.querySelector('#btUpdate')){
+	document.querySelector('#btUpdate').addEventListener('click', onUpdate)
+	function onUpdate(e) {
+		location.href = '/'+this.dataset['lang']+'/book/form/' + this.dataset['idx'];
+	}
 }
